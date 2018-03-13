09f587a567082f9e77d8ea328591b811708f2c1c
t-g-j <t.g.j@sol.dk>
2018-03-13 15:04:37 +0100

    Migrated to Pycharm IDE

--- VideoTracker.py
+++ VideoTracker.py
@@ -11,11 +11,13 @@ class VideoTracker:
         self.video = cv2.VideoCapture(video_path)
 
         self.clicked = False
-        self.blur = 1
-        self.kernelSize = 1
-        self.it0 = 0
-        self.it1 = 0
+        self.blur = 3
+        self.kernelSize = 3
+        self.it0 = 3
+        self.it1 = 25
         self.it2 = 0
+        self.coord2Track
+
 
     # Function that is called whenever the mouse is clicked. Remember to set callback.
     def on_mouse(self, event, x, y, flag, param):
@@ -196,6 +198,7 @@ class VideoTracker:
         new_window_name = window_name + ": " + str(num_labels)
         cv2.setWindowTitle(window_name, new_window_name)
 
+
     # Display both background subtraction and blobs detected
     def run_background_and_blobs(self, window_name_background, window_name_blobs, use_transformation=False):
         has_completed_one_cycle = False
@@ -216,3 +219,40 @@ class VideoTracker:
                 break
 
             has_completed_one_cycle = True
+
+
+    def opticalFlowTracking(self, pointArray2Track):
+        #### Optical flow defines ###
+        lk_params = dict(winSize=(15, 15),
+                              maxLevel=2,
+                              criteria=(cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
+        p0 = pointArray2Track
+        while(True):
+            ret, frame = self.video.read()
+            ret, old_frame = self.video.read()
+            old_gray = cv2.cvtColor(old_frame, cv2.COLOR_BGR2GRAY)
+            color = (150, 100, 3)
+            mask = np.zeros_like(old_frame)
+
+
+            frame_grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            p1, st, err = cv2.calcOpticalFlowPyrLK(old_gray, frame_grey, p0, None, **lk_params)
+            # Select good points
+            good_new = p1[st == 1]
+            good_old = p0[st == 1]
+            # Drawing the points
+            for i, (new, old) in enumerate(zip(good_new, good_old)):
+                a, b = new.ravel()
+                c, d = old.ravel()
+                mask = cv2.line(mask, (a, b), (c, d), color[i].tolist(), 2)
+                frame = cv2.circle(frame, (a, b), 5, color[i].tolist(), -1)
+            img = cv2.add(frame, mask)
+            cv2.imshow(img, "frame")
+            old_gray = frame_grey.copy()
+            p0 = good_new.reshape(-1, 1, 2)
+            cv2.destroyAllWindows()
+            self.video.release()
+            k = cv2.waitKey(1) & 0xff
+            if k == 27:
+                break
+
