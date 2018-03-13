7b5e98694dab398ceadaca0510172126b9b17ee7
andkgl <angle14@student.sdu.dk>
2018-03-13 15:03:44 +0100

    Lots of stuff

--- VideoTracker.py
+++ VideoTracker.py
@@ -1,6 +1,8 @@
 import cv2
+
 import numpy as np
 from threading import Thread
+import sys
 
 
 class VideoTracker:
@@ -11,11 +13,16 @@ class VideoTracker:
         self.video = cv2.VideoCapture(video_path)
 
         self.clicked = False
-        self.blur = 1
-        self.kernelSize = 1
-        self.it0 = 0
-        self.it1 = 0
+        self.blur = 3
+        self.kernelSize = 3
+        self.it0 = 3
+        self.it1 = 25
         self.it2 = 0
+
+        self.newFrameReady = False
+        self.currentFrame = 0
+
+        self.tracker_array = []
 
     # Function that is called whenever the mouse is clicked. Remember to set callback.
     def on_mouse(self, event, x, y, flag, param):
@@ -25,7 +32,7 @@ class VideoTracker:
             self.clicked = True
 
     # Click on four points in a square to make a transformation.
-    def click_to_make_transformation(self, window_name, frame_number = 0):
+    def click_to_make_transformation(self, window_name, frame_number=0):
         self.video.set(cv2.CAP_PROP_POS_FRAMES, frame_number)
 
         ret, image = self.video.read()
@@ -109,7 +116,6 @@ class VideoTracker:
             old_background_thresh = background_thresh
             old_background_frames = background_frames
 
-
     # Start thread with trackbars
     def display_trackbars(self, window_name):
         thread = Thread(target=self.trackbars, args=(window_name,))
@@ -118,15 +124,14 @@ class VideoTracker:
     def create_background_subtractor(self, frames, threshold):
         self.backgroundSubtractor = cv2.createBackgroundSubtractorMOG2(frames, threshold, True)
 
-    def subtract_background(self, use_transformation=False):
-        ret, frame = self.video.read()
-
-        frame = cv2.blur(frame, (self.blur, self.blur))
+    # Maybe transform after background sub?
+    def subtract_background(self, frame, use_transformation=False):
+        use_frame = cv2.blur(frame, (self.blur, self.blur))
 
         if use_transformation:
-            frame = cv2.warpPerspective(frame, self.h, self.size)
+            use_frame = cv2.warpPerspective(use_frame, self.h, self.size)
 
-        fgmask = self.backgroundSubtractor.apply(frame)
+        fgmask = self.backgroundSubtractor.apply(use_frame)
 
         kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (self.kernelSize, self.kernelSize))
         fgmask = cv2.erode(fgmask, kernel, iterations=self.it0)
@@ -134,12 +139,13 @@ class VideoTracker:
         fgmask = cv2.erode(fgmask, kernel, iterations=self.it2)
 
         fgmask = cv2.cvtColor(fgmask, cv2.COLOR_GRAY2BGR)
-        return frame, fgmask
+        return use_frame, fgmask
 
     # Subtract the background.
     def run_background_subtractor(self, window_name, use_transformation=False):
         while True:
-            frame, background = self.subtract_background(use_transformation)
+            ret, frame = self.video.read()
+            frame, background = self.subtract_background(frame, use_transformation)
 
             display_image = cv2.addWeighted(frame, 0.5, background, 0.5, 0.0)
 
@@ -157,7 +163,7 @@ class VideoTracker:
             background = cv2.cvtColor(background, cv2.COLOR_BGR2GRAY)
 
             if has_completed_one_cycle:
-                self.blob_detection(window_name, background, frame, 4)
+                self.draw_blob_detection(window_name, background, frame, 4)
 
             k = cv2.waitKey(10) & 0xff
             if k == 27:
@@ -165,8 +171,19 @@ class VideoTracker:
 
             has_completed_one_cycle = True
 
-    # Use blob detection on an image and display the found blobs on another image.
-    def blob_detection(self, window_name, detect_image, display_image, connectivity=4):
+    # Convert array to a usable array for optical flow
+    def to_array_for_optical_flow(self, centroids):
+        p0 = np.array([[[0, 0]]], dtype=np.float32)
+        for i, val in enumerate(centroids):
+            if i == 0:
+                continue
+            pMouse = np.append(p0, [[[centroids[i][0], centroids[i][1]]]], axis=0)
+            p0 = np.array(pMouse, dtype=np.float32)
+
+        return p0
+
+    # Get info from blob detection
+    def blob_detection(self, detect_image, connectivity=4):
         ret, thresh = cv2.threshold(detect_image, 0, 255, cv2.THRESH_BINARY)
 
         # Perform the operation
@@ -181,6 +198,13 @@ class VideoTracker:
         stats = output[2]
         # The fourth cell is the centroid matrix
         centroids = output[3]
+
+        return num_labels, labels, stats, centroids
+
+    # Use blob detection on an image and display the found blobs on another image.
+    def draw_blob_detection(self, window_name, detect_image, display_image, connectivity=4):
+
+        num_labels, labels, stats, centroids = self.blob_dection(detect_image, connectivity)
 
         # Draw circles where blobs where found
         for i, val in enumerate(centroids):
@@ -200,7 +224,8 @@ class VideoTracker:
     def run_background_and_blobs(self, window_name_background, window_name_blobs, use_transformation=False):
         has_completed_one_cycle = False
         while True:
-            frame, background = self.subtract_background(use_transformation)
+            ret, frame = self.video.read()
+            frame, background = self.subtract_background(frame, use_transformation)
 
             display_image = cv2.addWeighted(frame, 0.5, background, 0.5, 0.0)
 
@@ -209,10 +234,109 @@ class VideoTracker:
             background = cv2.cvtColor(background, cv2.COLOR_BGR2GRAY)
 
             if has_completed_one_cycle:
-                self.blob_detection(window_name_blobs, background, frame, 8)
+                self.draw_blob_detection(window_name_blobs, background, frame, 8)
 
             k = cv2.waitKey(1) & 0xff
             if k == 27:
                 break
 
             has_completed_one_cycle = True
+
+    # Create and return a tracker
+    def create_tracker(self, tracker_nr, point, frame):
+        tracker_types = ['BOOSTING', 'MIL', 'KCF', 'TLD', 'MEDIANFLOW', 'GOTURN']
+        tracker_type = tracker_types[tracker_nr]
+
+        tracker = cv2.Tracker
+        if tracker_type == 'BOOSTING':
+            tracker = cv2.TrackerBoosting_create()
+        if tracker_type == 'MIL':
+            tracker = cv2.TrackerMIL_create()
+        if tracker_type == 'KCF':
+            tracker = cv2.TrackerKCF_create()
+        if tracker_type == 'TLD':
+            tracker = cv2.TrackerTLD_create()
+        if tracker_type == 'MEDIANFLOW':
+            tracker = cv2.TrackerMedianFlow_create()
+        if tracker_type == 'GOTURN':
+            tracker = cv2.TrackerGOTURN_create()
+
+        # Define an initial bounding box
+        bbox = (point[0], point[1], 30, 30)
+
+        ok = tracker.init(frame, bbox)
+
+        return tracker
+
+    def run_tracker(self, window_name, tracker_nr, use_transformation=False):
+        thread = Thread(target=self.tracker, args=(window_name,))
+        thread.start()
+
+        cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
+        cv2.setMouseCallback(window_name, self.on_mouse)
+
+        has_completed_one_cycle = False
+        while True:
+            # if self.clicked:
+            #     new_tracker = self.create_tracker(tracker_nr, (self.mouseX, self.mouseY), self.currentFrame)
+            #     self.tracker_array.append(new_tracker)
+            #     self.clicked = False
+
+            ret, self.currentFrame = self.video.read()
+
+            frame, background = self.subtract_background(self.currentFrame, use_transformation)
+
+            background = cv2.cvtColor(background, cv2.COLOR_BGR2GRAY)
+
+            if has_completed_one_cycle:
+                num_labels, labels, stats, centroids = self.blob_detection(background, 8)
+                for point in centroids:
+                    new_tracker = self.create_tracker(tracker_nr, point, self.currentFrame)
+                    self.tracker_array.append(new_tracker)
+                    break
+
+            self.newFrameReady = True
+
+            has_completed_one_cycle = True
+
+            cv2.waitKey(1)
+
+    def tracker(self, window_name):
+        # Set up tracker.
+
+        while True:
+            while not self.newFrameReady:
+                cv2.waitKey(1)
+
+            # Start timer
+            timer = cv2.getTickCount()
+
+            good_trackers = []
+            for tracker in self.tracker_array:
+                # Update tracker
+                ok, bbox = tracker.update(self.currentFrame)
+
+                # Draw bounding box
+                if ok:
+                    # Tracking success
+                    p1 = (int(bbox[0]), int(bbox[1]))
+                    p2 = (int(bbox[0] + bbox[2]), int(bbox[1] + bbox[3]))
+                    cv2.rectangle(self.currentFrame, p1, p2, (255, 0, 0), 2, 1)
+                    good_trackers.append(tracker)
+
+                # Exit if ESC pressed
+                k = cv2.waitKey(1) & 0xff
+                if k == 27:
+                    break
+
+            self.tracker_array = good_trackers
+
+            # Display result
+            cv2.imshow(window_name, self.currentFrame)
+            cv2.waitKey(1)
+
+            # Calculate Frames per second (FPS)
+            fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer)
+
+            # Display FPS on frame
+            cv2.putText(self.currentFrame, "FPS : " + str(int(fps)), (100, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50, 170, 50), 2)
